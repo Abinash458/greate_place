b64aff35c9e9ce8f56a575833db9025e806bc989
Abinash Mohapatra <abinash@expensebit.com>
2020-10-12 21:30:26 +0530

    navigation and screens setup

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const NewPlaceScreen = props => {
+    return (
+        <View>
+            <Text>Add New Place Screen</Text>
+        </View>
+    );
+}
+
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'Add Place'
+}
+
+const styles = StyleSheet.create({});
+
+export default NewPlaceScreen;
---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const PlaceDetailScreen = props => {
+    return (
+        <View>
+            <Text>Places Details Screen!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default PlaceDetailScreen;
